9193e15034d77a49431792d3b9f9256ad2abb859
richard miruka onsare <richardonsare15@gmail.com>
2023-06-07 13:00:17 +0300

    update my cli.py file with the correct import statements

--- app/database.py
+++ app/database.py
@@ -1,22 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from .models import Data
 
-# SQLite database file
-DATABASE_FILE = 'data.db'
 
-# SQLite connection URL
-DATABASE_URL = f'sqlite:///{DATABASE_FILE}'
+# Defining the database connection URL
+db_url = 'sqlite:///data.db'
 
-# Create the engine and bind the Base model to it
-engine = create_engine(DATABASE_URL)
-Base.metadata.bind = engine
-
-# Create all tables defined in the models
-Base.metadata.create_all()
+# Create the database engine
+engine = create_engine(db_url)
 
 # Create a session factory
 Session = sessionmaker(bind=engine)
-
 
     
