d4903dbad78ef582776da1f202819e207dda378b
richard miruka onsare <richardonsare15@gmail.com>
2023-06-07 16:20:57 +0300

    defined my Importer class

--- app/cli.py
+++ app/cli.py
@@ -5,85 +5,40 @@ from .database import Session
 from .models import Data
 from .utils import merge_sort
 
-@click.group()
-def cli():
-    """Data Import/Export CLI Application"""
-    pass
+class Importer:
+    def __init__(self, file_path, sort):
+        self.file_path = file_path
+        self.sort = sort
 
-@cli.command()
-@click.argument('file_path')
-@click.option('--sort', '-s', is_flag=True, help='Sort the data before importing')
-def import_data(file_path, sort):
-    """
-    Import data from a CSV file into the database
-    
-    Args:
-        file_path (str): Path to the CSV file
-        sort (bool): Flag to enable sorting of data before importing
-    """
-    data = []
+    def run(self):
+        data = []
 
-    with open(file_path, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            data.append(row)
+        with open(self.file_path, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                data.append(row)
 
-    if sort:
-        column1_values = [row['column1'] for row in data]
-        sorted_column1 = merge_sort(column1_values)
-        for i, row in enumerate(data):
-            row['column1'] = sorted_column1[i]
+        if self.sort:
+            column1_values = [row['column1'] for row in data]
+            sorted_column1 = merge_sort(column1_values)
+            for i, row in enumerate(data):
+                row['column1'] = sorted_column1[i]
 
+        session = Session()
 
-    session = Session()
+        try:
+            for row in data:
+                entry = Data(column1=row['column1'], column2=row['column2'], column3=row['column3'])
+                session.add(entry)
 
-    try:
-        for row in data:
-            entry = Data(column1=row['column1'], column2=row['column2'], column3=row['column3'])
-            session.add(entry)
+            session.commit()
 
-        session.commit()
+            click.echo("Data import successful!")
 
-        click.echo("Data import successful!")
+        except Exception as e:
+            session.rollback()
+            click.echo(f"Data import failed: {str(e)}", err=True)
 
-    except Exception as e:
-        session.rollback()
-        click.echo(f"Data import failed: {str(e)}", err=True)
-
-    finally:
-        session.close()
-
-@cli.command()
-@click.argument('file_path')
-def export_data(file_path):
-    """
-    Export data from the database to a CSV file
-    
-    Args:
-        file_path (str): Path to the CSV file for exporting the data
-    """
-    session = Session()
-
-    try:
-        data = session.query(Data).all()
-
-        csv_data = []
-        for entry in data:
-            row = [entry.column1, entry.column2, entry.column3]
-            csv_data.append(row)
-
-        with open(file_path, 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(['column1', 'column2', 'column3'])
-            csv_writer.writerows(csv_data)
-
-        click.echo("Data export successful!")
-
-    except Exception as e:
-        click.echo(f"Data export failed: {str(e)}", err=True)
-
-    finally:
-        session.close()
-
-
+        finally:
+            session.close()
 
